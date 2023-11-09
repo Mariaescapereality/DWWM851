a75d01072def5c30756a2323f4164137217abe89
Mariaescapereality <145456229+Mariaescapereality@users.noreply.github.com>
2023-11-09 14:10:15 +0100

    Add files via upload

---
+++ 03-javascript/12-module/salutation.js
@@ -0,0 +1,29 @@
+"use strict";
+/**
+ * Affiche message de salutation
+ */
+export default function bonjour(){
+    console.log("Bonjour les gens !")
+}
+/**
+ * Affiche message de salutation
+ */
+export default function salut(){
+    console.log("Salut la populace !")
+}
+/**
+ * Affiche message de salutation
+ * @param {string} name Nom de personne
+ */
+function bonjour(){
+    console.log("Bonjour les gens !")
+}
+/**
+ * Affiche message de salutation
+ */
+export default function coucou(name){
+    parler(name, "Coucou les gens !")
+}
+/**
+ * Affiche message de salutation
+ */
