31de9d92103efc0187512a596080501dc4818da6
Mariaescapereality <145456229+Mariaescapereality@users.noreply.github.com>
2023-11-24 11:49:20 +0100

    Add files via upload

---
+++ docker/01-dev/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker</title>
+    <script src="./script.js" defer></script>
+</head>
+<body>
+   <h1>Bravo, vous faites tourner un conteneur Docker.</h1>
+   
+</body>
+</html>
